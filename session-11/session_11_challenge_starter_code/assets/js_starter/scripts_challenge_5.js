23663e004a5504543dfcf1b6aa778a055ce0ff6d
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-01 11:23:59 +0800

    feat: kind of complete challenge 5

--- session-11/session_11_challenge_starter_code/assets/js_starter/scripts_challenge_5.js
+++ session-11/session_11_challenge_starter_code/assets/js_starter/scripts_challenge_5.js
@@ -1,14 +1,25 @@
 // Challenge 5 Starter
-const sidebar = document.querySelector('.sidebar');
+const sidebar = document.querySelector(".sidebar");
 
 function toggleSidebarContent() {
   // Your code goes here
+  // check if sidebar list has children
+  if (sidebar.hasChildNodes()) {
+    // remove first child
+    sidebar.removeChild(sidebar.firstChild);
+    // create a <p> element
+    const newElement = document.createElement("p");
+
+    newElement.textContent = "AAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    // add it to the sidebar
+    sidebar.appendChild(newElement);
+  }
 }
 
 // Add a button to toggle the sidebar content
-const toggleButton = document.createElement('button');
-toggleButton.textContent = 'Toggle Sidebar Content';
-toggleButton.addEventListener('click', toggleSidebarContent);
+const toggleButton = document.createElement("button");
+toggleButton.textContent = "Toggle Sidebar Content";
+toggleButton.addEventListener("click", toggleSidebarContent);
 
 // Append the button to the body
 document.body.appendChild(toggleButton);
