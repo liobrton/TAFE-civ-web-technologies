03f9d850f84c46f66dfb1eab56a3258499aceb3d
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-01 10:29:58 +0800

    feat: complete challenge 4

--- session-11/session_11_challenge_starter_code/assets/js_starter/scripts_challenge_4.js
+++ session-11/session_11_challenge_starter_code/assets/js_starter/scripts_challenge_4.js
@@ -1,8 +1,13 @@
 // Challenge 4 Starter
-const eventLinks = document.querySelectorAll();
+const eventLinks = document.querySelectorAll("[data-event]"); //finds all elements that have the data-event attribute
+// [ ] is the attribute selector
 
 function logEvent(eventDetails) {
-    // Your code goes here
+  //code goes here
 }
 
-// Your code goes here
+for (const [eventDetails] of eventLinks.entries()) {
+  //[eventDetails] is the index, the first item in eventLinks.entries()
+  console.log(`${eventLinks[eventDetails].innerHTML.toUpperCase()}`);
+  // innerHTML attribute grabs the text on the inside??
+}
