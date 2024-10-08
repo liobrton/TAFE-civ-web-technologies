a3082880d046f67a149fd0af8b75e79d687852e1
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-08 10:48:00 +0800

    rebase session content 2

--- session-12/assets/js_starter/scripts_challenge_2.js
+++ session-12/assets/js_starter/scripts_challenge_2.js
@@ -4,12 +4,16 @@ let array1 = [1, 2, 3, 2];
 let array2 = [4, 5, 6, 4];
 
 // Use the spread operator to merge the two arrays
-let mergedArray = [...array1, ...array2];
+
+let merged_array = [...array1, ...array2];
 
 // Use the Set object to remove duplicates
-let uniqueArray = [...new Set(mergedArray)];
+let no_duplicates = new Set(merged_array);
+let no_duplicates_array = Array.from(no_duplicates); //creates array from iterable object
+
 // Use array destructuring to assign the first three elements to variables
-let [first, second, third, ...others] = uniqueArray;
+const [value1, value2, value3, ...others] = no_duplicates_array;
+
 // Set the value of the output filed to be the second element of the unique array
-let outField = document.getElementById("OutputField");
-outField.innerHTML = uniqueArray;
+let outputfield = document.getElementById("OutputField");
+outputfield.innerHTML = `Second element of unique array: ${no_duplicates_array[1]}`;
