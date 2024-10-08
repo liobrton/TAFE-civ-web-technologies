69281b55dbf3db09015335934d99036e1d59e81d
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-08 10:45:44 +0800

    feat: progress on challenge 5

--- session-12/assets/js_starter/scripts_challenge_5.js
+++ session-12/assets/js_starter/scripts_challenge_5.js
@@ -3,8 +3,10 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Define the function doubleNumbers
+function doubleNumbers(array_of_numbers) {}
 
 // Call the function with the numbers array
+const result = doubleNumbers(numbers);
 
 // Console log the new array
 console.log(result);
