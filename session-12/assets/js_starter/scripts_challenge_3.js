6c210220bce40b8cb71d464ca0f145963c6a1e0b
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-08 10:48:38 +0800

    rebase session content 3

--- session-12/assets/js_starter/scripts_challenge_3.js
+++ session-12/assets/js_starter/scripts_challenge_3.js
@@ -5,17 +5,16 @@ let numbers = [1, 2, 3, 4, 5, 6];
 let number = 5;
 
 // Check if the array includes the number 5
-if (numbers.includes(number)) {
-    // If true, filter out numbers less than 5
-    let filteredNumbers = numbers.filter(number => number*5);
+if (numbers.includes(5)) {
+  // If true, filter out numbers less than 5
+  let filtered = numbers.filter((item) => item >= 5);
+  console.log(filtered);
+  
+  // Then square each number in the resulting array
+  let mapped = filtered.map((item) => item ** 2);
 
-    // Then square each number in the resulting array
-    let squaredNumbers = numbers.map(number => number*number);
-
-    // Output the final array
-    console.log(`Squred Number: ${squaredNumbers}, Filtered Numbers: ${filteredNumbers}`);
-
+  // Output the final array
+  console.log(`Final array: ${mapped}.`);
 } else {
-    // Output a message if the number 5 is not in the array
-    console.log(`The number ${number} is not in the array`);
-}
+  // Output a message if the number 5 is not in the array
+  console.log("There is not a 5 in the array.");
