d0305655c541e09285048232d344641db9429565
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-15 10:12:10 +0800

    feat: challenge 3 complete

--- session-13/assets/js_starter/scripts_challenge_3.js
+++ session-13/assets/js_starter/scripts_challenge_3.js
@@ -1,9 +1,18 @@
 // Challenge 3 Starter
 function safeDivide(a, b) {
-    return a / b;
+  try {
+    // if b is 0 throw error with user friendly msg
+    if (b === 0) {
+      throw new Error("division by 0 is illegal what are you doing");
+    }
+
+    const result = a / b;
+
+    return result;
+  } catch (error) {
+    console.error(`An unexpected error occurred: ${error.message}`);
+  }
 }
 
 console.log(safeDivide(10, 0)); // Expected Output: "An error occurred: Division by zero is not allowed."
 console.log(safeDivide(10, 2)); // Expected Output: 5
-
-
