61f9b869ae40fb4082e61e9b95e9d8b2accadc89
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-15 09:44:09 +0800

    feat: complete challenge 1

--- session-13/assets/js_starter/scripts_challenge_1.js
+++ session-13/assets/js_starter/scripts_challenge_1.js
@@ -1,6 +1,33 @@
 // Challenge 1 Starter
 // Event listener for button 1
+// click event listener changes the button's text to 'Clicked!' when it's clicked
+
+// get the button
+const click_button = document.getElementById("button1");
+
+// create the function to be executed
+function clickedText(event) {
+  this.textContent = "Clicked!";
+}
+
+// attach event handler
+click_button.addEventListener("click", clickedText);
 
 // Event listener for button 2
+// a mouseover event listener displays an alert when the mouse is hovered over the button
+const hover_button = document.getElementById("button2");
+
+function displayAlert(event) {
+  console.log("hello world!");
+  alert("Congrats for hovering over the button i guess");
+}
+
+hover_button.addEventListener("mouseover", displayAlert);
 
 // Event listener for button 3
+// a double-click event listener logs a message to the console when the button is double clicked
+
+function logMessage(event) {
+  console.log("Logging message, you double clicked on the button!");
+}
+document.getElementById("button3").addEventListener("dblclick", logMessage);
