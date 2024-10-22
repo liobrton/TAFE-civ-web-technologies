38b26dd92003a527d8f62885a193d7c3942d007b
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-22 09:22:06 +0800

    feat: complete challenge 1

---
+++ session-14/assets/js/scripts_challenge_1.js
@@ -0,0 +1,29 @@
+// Challenge 1 Starter
+const userObject = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isStudent: false,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    zipCode: "12345",
+  },
+  skills: ["JavaScript", "HTML", "CSS"],
+  socialProfiles: {
+    twitter: "@johndoe",
+    linkedIn: "linkedin.com/in/johndoe",
+  },
+  isEmployed: true,
+
+  // Method to change the age property
+  changeAge: function (newAge) {
+    this.age = newAge;
+  },
+};
+
+// Execute the method to change the age
+userObject.changeAge(31);
+
+// Log the updated complexObject
+console.log(`New age: ${userObject.age}`);
