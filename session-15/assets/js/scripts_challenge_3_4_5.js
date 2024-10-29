8e5834b169c703547511b4261bbb1b6b31178882
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-29 10:07:06 +0800

    feat: complete challenge 3

--- session-15/assets/js/scripts_challenge_3_4_5.js
+++ session-15/assets/js/scripts_challenge_3_4_5.js
@@ -1,20 +1,36 @@
 // Challenge 3
 function originalFunction() {
     // Start fetching data from the server
-    fetch('https://jsonplaceholder.typicode.com/posts/') // Step 1: Initiate an HTTP request
+    fetch("https://jsonplaceholder.typicode.com/posts/") // Step 1: Initiate an HTTP request
         .then(function (response) {
             // Step 2: Wait for the response to arrive
             return response.json(); // Step 3: Parse the response as JSON
         })
         .then(function (data) {
             // Step 4: Handle the parsed JSON data
-            console.log('Traditional Fetch', data); // Step 5: Display the data
+            console.log("Traditional Fetch", data); // Step 5: Display the data
         });
 
     // This message will be displayed immediately
-    console.log('Traditional Message'); // Step 6: Display this message
+    console.log("Traditional Message"); // Step 6: Display this message
 }
 
 originalFunction();
 
-
+async function newFunction(id) {
+    try {
+        // Start fetching data from the server
+        // Step 1: Initiate an HTTP request
+        // Step 2: Wait for the response to arrive
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        // Step 3: Parse the response as JSON
+        // Step 4: Handle the parsed JSON data
+        const data = await response.json();
+        // Step 5: Display the data
+        console.log(data);
+    } catch (error) {
+        console.error(`An error ocurred: ${error}`);
+    }
+}
