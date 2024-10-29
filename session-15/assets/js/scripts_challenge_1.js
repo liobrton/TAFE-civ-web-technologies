ba800d3d8beac28946341d3498f872b6ec155719
Eliot Barton <20124796@tafe.wa.edu.au>
2024-10-29 09:34:05 +0800

    feat: complete 2/3 things for challenge 1

--- session-15/assets/js/scripts_challenge_1.js
+++ session-15/assets/js/scripts_challenge_1.js
@@ -1,22 +1,25 @@
 // Challenge 1
 // Import the array of books
-import books from "/books.js"
+import books from "./books.js";
 
 // Find a book by ISBN
 const findBookByISBN = (ISBN) => {
     // Use the find function to search for a ISBN number (checking against book.ISBN)
-
+    const foundBook = books.find((book) => book.ISBN === ISBN);
     // If found log a title
-
+    if (foundBook) {
+        console.log(foundBook);
+    } else {
+        console.log(`No book with that ISBN (${ISBN}) was found.`);
+    }
 };
 
 // Double the prices of all books
 const doublePrices = () => {
     // Create a constant which takes the result of mapping over the books
-
-        // Use each iteration to double the price of the book 
-        // adding it to doubledPrices
-
+    // Use each iteration to double the price of the book
+    // adding it to doubledPrices
+    const doubledPrices = books.map((book) => ({ price: book.price * 2 }));
 
     console.log("Doubled prices:", doubledPrices);
 };
@@ -24,7 +27,6 @@ const doublePrices = () => {
 // Calculate the total price of all books
 const calculateTotalPrice = () => {
     // Create a constant that take the result of the reduce function
-
     // Also add a total accumulator to the reduce function arguments
 
     console.log("Total price of all books:", totalPrice);
@@ -34,6 +36,3 @@ const calculateTotalPrice = () => {
 findBookByISBN("ISBN-222");
 doublePrices();
 calculateTotalPrice();
-
-
-
